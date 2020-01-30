f372149f8f0e00bbd3f4a4a8a3efbd13a6f7fd3e
Daniel Carrizales <rated223@outlook.com>
2020-01-30 00:02:54 -0600

    Applying thunk middleware
    Signed-off-by: Daniel Carrizales <rated223@outlook.com>

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { expensesReducer, filtersReducer } from '../reducers';
+import thunk from 'redux-thunk';
 
 export default () => {
   const store = createStore(
     combineReducers({
       expenses: expensesReducer,
       filters: filtersReducer
-    })
+    }),
+    applyMiddleware(thunk)
   );
   
   return store;
